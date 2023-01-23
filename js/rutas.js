916c9c3abcf5fa1e5af60489858c163d3195c8d2
cronoser <oscar_zorrilla@hotmail.com>
2023-01-23 19:22:36 +0100

    añadido visualizar rutas

---
+++ js/rutas.js
@@ -0,0 +1,32 @@
+function addRutas() {
+
+    var url = 'datos/rutas.geojson';
+    map.addSource('rutas', { type: 'geojson', data: url});
+
+         map.addLayer({
+    'id': 'route',
+    'type': 'line',
+    'source': 'rutas',
+    'layout': {
+    'line-join': 'round',
+    'line-cap': 'round'
+    },
+    'paint': {
+    'line-color': '#ff0000',
+    'line-width': 3
+    }
+    });
+
+} //fin funcion
+
+//estos es un ejemplo de valores que se le pasan (lat,long, zoom) 12.96/41.76589/2.30274
+function zoomToRutas(valores) {
+
+    var coord = valores.split("/"); //parte el valor en un array con 3 posiciones
+
+    map.flyTo({
+            center: [coord[2], coord[1]],
+            zoom: coord[0]
+        });
+
+} //fin funcion
