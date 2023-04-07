4a8f631d8870a927a47e56b95f7ceb2d999ad140
cronoser <oscar_zorrilla@hotmail.com>
2023-04-07 17:06:39 +0200

    loberas
    actualizados descripcion loberas

--- js/loberas.js
+++ js/loberas.js
@@ -1,25 +1,26 @@
 var config = {
-    style: 'mapbox://styles/mapbox/satellite-v9',
+    style: 'mapbox://styles/cronoser1/clg6hi5hh002101qx9ovtvu3a',
+    // style: 'mapbox://styles/mapbox/satellite-v9', //estilo original
     accessToken: 'pk.eyJ1IjoiY3Jvbm9zZXIxIiwiYSI6ImNsY3oxbmM4dTJpdnczcm1zN3Exc3RyMDcifQ.UYUlgPFANSfaK4YmcUDF1g',
     showMarkers: false,
     markerColor: '#3FB1CE',
     theme: 'dark',
     use3dTerrain: true,
     title: 'Loberas',
-    subtitle: 'Descubre las loberas de la comarca de las Merindades',
+    subtitle: 'Un paseo por las loberas de la comarca de Las Merindades (Bugos)',
     byline: 'M2B ',
     footer: 'Source: Wikipedia',
     chapters: [
         {
-            id: 'volcan1',
+            id: 'lobera1',
             alignment: 'left',
             hidden: false,
-            title: 'Eyjafjallajökull, Islandia',
-            image: 'https://upload.wikimedia.org/wikipedia/commons/e/e4/Eyjafjallaj%C3%B6kull_first_crater_20100329.jpg',
+            title: 'Lobera Monte Santiago',
+            image: 'https://www.lasmerindades.com/sites/merindades/files/styles/slide_cabecera/public/lobera.jpg',
             description: 'También conocido como Eyjafjöll,2​ o Eyjafjalla,3​ es un volcán situado al norte de Skógar, en la región de Suðurland, al sur de Islandia.4​ Tiene entre 1651 m y 1666 m de altitud,​ y ha estado moderadamente activo en los últimos 8000 años.',
             location: {
-                center: [-19.62, 63.63],
-                zoom: 13.5,
+                center: [-2.9838734, 42.940537],
+                zoom: 16,
                 pitch: 60,
                 bearing: 0
             },
@@ -40,17 +41,17 @@ var config = {
                 // }
             ]
         },{
-            id: 'volcan2',
+            id: 'lobera2',
             alignment: 'right',
             hidden: false,
-            title: 'Monte Vesubio, Italia',
-            image: 'https://upload.wikimedia.org/wikipedia/commons/2/24/Cr%C3%A1ter_del_Vesubio.jpg',
+            title: 'Lobera de la Barrerilla',
+            image: 'https://3.bp.blogspot.com/-9VwQP5A_8Zs/Vk-kMftrfQI/AAAAAAAABc8/U71U3IDuggc/s640/lobera%2Bde%2Bla%2Bbarrerilla%2B3.JPG',
             description: 'Es un volcán activo del tipo vesubiano situado frente a la bahía de Nápoles y a unos nueve kilómetros de distancia de la ciudad de Nápoles. Se encuentra en la ciudad metropolitana de Nápoles, perteneciente a la región italiana de la Campania. Tiene una altura máxima de 1281 m s. n. m. y se alza al sur de la cadena principal de los Apeninos.',
             location: {
-                center: [14.42555555, 40.82138888],
-                zoom: 14.5,
+                center: [-3.2940803, 42.9545148],
+                zoom: 16,
                 pitch: 60,
-                bearing: -43.2
+                bearing: 0
             },
             mapAnimation: 'flyTo',
             rotateAnimation: true,
