ef64af60a7e2975df91ff8b5a5aba57a4fe8622c
cronoser <oscar_zorrilla@hotmail.com>
2023-04-11 14:56:49 +0200

    Update loberas.js
    habilitados marcadores

--- js/loberas.js
+++ js/loberas.js
@@ -1,8 +1,8 @@
 var config = {
     style: 'mapbox://styles/mapbox/satellite-v9',
     accessToken: 'pk.eyJ1IjoiY3Jvbm9zZXIxIiwiYSI6ImNsY3oxbmM4dTJpdnczcm1zN3Exc3RyMDcifQ.UYUlgPFANSfaK4YmcUDF1g',
-    showMarkers: false,
-    markerColor: '#3FB1CE',
+    showMarkers: true,
+    markerColor: '#138d75',
     theme: 'dark',
     use3dTerrain: true,
     title: 'Loberas',
@@ -16,7 +16,7 @@ var config = {
             hidden: false,
             title: 'Lobera Monte Santiago',
             image: 'https://www.lasmerindades.com/sites/merindades/files/styles/slide_cabecera/public/lobera.jpg',
-            description: 'Dentro del Monumento Natural Monte Santiago y junto a la cáscada mas alta de España. Se trata de una lobera excepcional, ya que uno de los muros da lugar a dos loberas, una en cada extremo. Se trata de la mas grande de una total de 4 loberas en el mismo enclave.',
+            description: 'Dentro del Monumento Natural Monte Santiago y junto a la cáscada mas alta de España. Se trata de una lobera excepcional, ya que uno de los muros da lugar a dos loberas, una en cada extremo. Se trata de la mas grande de una total de cuatro loberas en el mismo enclave.',
             video: src='https://www.youtube.com/watch?v=nDleb4WGeqQ',
             location: {
                 center: [-2.9855180842918685, 42.936710456036366],
@@ -46,7 +46,7 @@ var config = {
             hidden: false,
             title: 'Lobera de San Miguel',
             image: 'https://3.bp.blogspot.com/-tcD4FfL14cg/WRGIlavOM1I/AAAAAAAAYSw/hL77ACxsDIYtpEaV_CqdDpywFypOxPAzwCKgB/s1600/lobera%2Bsan%2Bmiguel%2B3.jpg',
-            description: 'Situada en la misma sierra que la lobera de Monte Santiago pero unos kilómetros al oeste encontramos los restos no muy bien conservados de una antigua lobera que coge su nombre de la cueva de San Miguel el viejo.',
+            description: 'Situada en la misma sierra que la lobera de Monte Santiago pero a unos kilómetros al oeste encontramos los restos no muy bien conservados de una antigua lobera que coge su nombre de la cueva de San Miguel el viejo.',
             location: {
                 center: [-3.1722287459473932, 43.01701514669193],
                 zoom: 18,
@@ -64,7 +64,7 @@ var config = {
             hidden: false,
             title: 'Lobera de la Barrerilla',
             image: 'https://3.bp.blogspot.com/-9VwQP5A_8Zs/Vk-kMftrfQI/AAAAAAAABc8/U71U3IDuggc/s640/lobera%2Bde%2Bla%2Bbarrerilla%2B3.JPG',
-            description: '<iframe width="100%" height= "100%" src="https://www.youtube.com/embed/cLnal5LroJQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay=1; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>',
+            description: '<iframe width="100%" height="100%" src="https://www.youtube.com/embed/cLnal5LroJQ?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>',
             location: {
                 center: [-3.2940803, 42.9545148],
                 zoom: 17,
